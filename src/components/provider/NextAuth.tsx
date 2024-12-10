7c46e8bff1972372c84d288eafb2cab695bef1c8
Samuel Seibidor <samrythmn@gmail.com>
2024-12-10 21:02:16 +0100

    fix: Next Auth Config

--- src/components/provider/NextAuth.tsx
+++ src/components/provider/NextAuth.tsx
@@ -3,7 +3,17 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 
 const NextAuthProvider = ({ children }: LayoutProps) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider
+      baseUrl={
+        process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/auth`
+          : undefined
+      }
+    >
+      {children}
+    </SessionProvider>
+  );
 };
 
 export default NextAuthProvider;
