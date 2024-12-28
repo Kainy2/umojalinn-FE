59c32c5ace47d09f24cd58ef05034854213edc4e
Samuel Seibidor <samrythmn@gmail.com>
2024-12-28 19:01:31 +0100

    fix: reload

--- src/app/onboard/congratulations/page.tsx
+++ src/app/onboard/congratulations/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import useWindowSize from "@/hooks/useWindowSize";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 import Confetti from "react-confetti";
 
 const CongratulationsPage = () => {
   const { height, width } = useWindowSize();
+  const { data: session } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    const reload = () => {
+      if (!session?.user?.profileRole) {
+        router.refresh();
+      }
+    };
+    const timer = setTimeout(reload, 2000);
+    return () => clearTimeout(timer);
+  }, [router, session?.user?.profileRole]);
+
   return (
     <div className="bg-auth">
       <Confetti
@@ -32,9 +46,11 @@ const CongratulationsPage = () => {
           alt=""
           className="object-cover"
         />
-        <Button asChild fullWidth className="max-w-screen-sm">
-          <Link href="/">Continue</Link>
-        </Button>
+        {session?.user?.designerProfile && (
+          <Button asChild fullWidth className="max-w-screen-sm">
+            <Link href="/">Continue</Link>
+          </Button>
+        )}
       </div>
     </div>
   );
