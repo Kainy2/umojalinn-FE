db7be040e0b52ad31abe147919ef4462ec947773
Samuel Seibidor <samrythmn@gmail.com>
2025-01-09 17:19:28 +0100

    feat: view sizing template cards

--- src/app/(dashboard)/(corner)/sizing-templates/page.tsx
+++ src/app/(dashboard)/(corner)/sizing-templates/page.tsx
@@ -1,10 +1,23 @@
+"use client";
 import CreateSizingTemplateDialog from "@/components/custom/dialog/CreateSizingTemplate";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetAllSizingTemplates } from "@/tanstack/hooks/useSizingTemplates";
 import Image from "next/image";
 import React from "react";
 
 const SizingTemplatesPage = () => {
+  const { data, isPending } = useGetAllSizingTemplates();
+
+  if (isPending) {
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {new Array(4).fill("").map((_, index) => (
+        <Skeleton className="h-52" key={index} />
+      ))}
+    </div>;
+  }
+
   return (
-    <div className="grid grid-cols-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       <CreateSizingTemplateDialog>
         <button className="relative h-52">
           <Image
@@ -18,6 +31,23 @@ const SizingTemplatesPage = () => {
           </span>
         </button>
       </CreateSizingTemplateDialog>
+      {data?.data?.data?.map?.((template) => {
+        return (
+          <button className="relative h-52" key={template?.id}>
+            <Image
+              src="/public/img/webp/sizing-template-card.webp"
+              alt=""
+              className="absolute object-center object-cover"
+              fill
+            />
+            <div className="absolute bottom-0 p-4 backdrop-blur-md bg-white/30 border-t-1 border-white/50">
+              <h2 className="text-subtitle-2 font-bold text-center truncate w-full">
+                {template?.name}
+              </h2>
+            </div>
+          </button>
+        );
+      })}
     </div>
   );
 };
