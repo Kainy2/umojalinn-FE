748ab39b0cdf31d78a0add10a72c316ffe77b6b8
Samuel Seibidor <samrythmn@gmail.com>
2025-01-13 17:10:12 +0100

    feat: hide bid created

--- src/app/(dashboard)/(with-back-button)/jobs/[id]/page.tsx
+++ src/app/(dashboard)/(with-back-button)/jobs/[id]/page.tsx
@@ -23,7 +23,11 @@ const JobPage = () => {
 
   const { data: session } = useSession();
 
-  const { mutate: createBid, isPending: isCreatingBid } = useCreateBid({
+  const {
+    mutate: createBid,
+    isPending: isCreatingBid,
+    isSuccess: bidCreated,
+  } = useCreateBid({
     onSuccess(data) {
       router.push(`/bids/${uuidToBase62Safe(data?.data?.data?.id)}/edit`);
       toast({ description: "Please wait" });
@@ -97,16 +101,17 @@ const JobPage = () => {
             <Button variant="outline">
               <MoreVertical />
             </Button>
-            {!project?.bids?.length &&
-              session?.user?.profileRole === "DESIGNER" && (
-                <Button
-                  variant="default"
-                  onClick={() => createBid(id)}
-                  loading={isCreatingBid}
-                >
-                  Create Bid
-                </Button>
-              )}
+            {!bidCreated ||
+              (!project?.bids?.length &&
+                session?.user?.profileRole === "DESIGNER" && (
+                  <Button
+                    variant="default"
+                    onClick={() => createBid(id)}
+                    loading={isCreatingBid}
+                  >
+                    Create Bid
+                  </Button>
+                ))}
           </div>
         </div>
       </div>
