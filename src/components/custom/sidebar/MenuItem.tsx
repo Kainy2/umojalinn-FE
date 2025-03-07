d835fb1694370d8485b0e56878ad1115c31bd761
Samuel Seibidor <69042240+psalm987@users.noreply.github.com>
2025-03-07 22:01:43 +0100

    fix: hide ad on mobile

--- src/components/custom/sidebar/MenuItem.tsx
+++ src/components/custom/sidebar/MenuItem.tsx
@@ -67,7 +67,7 @@ const CustomSidebarMenuItem = (props: CustomSidebarMenuItemProps) => {
           src={props.img}
           height={172.72}
           width={216}
-          className="h-32 w-full object-cover rounded-md mb-6"
+          className="h-32 w-full object-cover rounded-md mb-6 hidden md:block"
         />
         <a className="font-semibold text-primary" href={props.action?.href} target='_blank'>
           {props.action?.title}
