04e9f6c82d67bae6367052b55d84eb046bc62db8
droxy <chiomaotu22@gmail.com>
2025-03-12 08:51:49 +0100

    fix: view project in work history

--- src/app/(dashboard)/(corner)/settings/(with-breadcrumbs)/profile/work-history/page.tsx
+++ src/app/(dashboard)/(corner)/settings/(with-breadcrumbs)/profile/work-history/page.tsx
@@ -4,9 +4,13 @@ import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { formatCurrencyValue } from "@/lib/number";
 import { getCurrencySymbol } from "@/lib/string";
+import { cn } from "@/lib/utils";
+import { uuidToBase62Safe } from "@/lib/uuid";
 import { useGetUserReviews } from "@/tanstack/hooks/useProject";
+import { UmojaLinnProject } from "@/types/project";
 import { formatDate } from "date-fns";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
 
 const SettingsProfileWorkHistoryPage = () => {
@@ -17,8 +21,14 @@ const SettingsProfileWorkHistoryPage = () => {
     },
     {
       enabled: !!session?.user?.profileRole,
-    }
+    },
   );
+
+  const getProjectHref = (id?: string, status?: UmojaLinnProject["status"]) => {
+    if (!id || !status) return null;
+    const baseHref = status === "COMPLETED" ? "completed-jobs" : "active-jobs";
+    return `/${baseHref}/${uuidToBase62Safe(id)}`;
+  };
 
   if (isPending)
     return (
@@ -38,37 +48,51 @@ const SettingsProfileWorkHistoryPage = () => {
 
   return (
     <div className="flex flex-col gap-8">
-      {userReviews?.data?.data?.map((review) => (
-        <div
-          key={review?.id}
-          className="border border-input p-4 text-foreground-body"
-        >
-          <h3 className="text-subtitle-2 text-foreground font-semibold mb-2.5">
-            {review?.project?.title}
-          </h3>
+      {userReviews?.data?.data?.map((review) => {
+        const href = getProjectHref(
+          review?.project?.id,
+          review?.project?.status,
+        );
+        return (
+          <div
+            key={review?.id}
+            className="border border-input p-4 text-foreground-body"
+          >
+            <h3 className="text-subtitle-2 text-foreground font-semibold mb-2.5">
+              {review?.project?.title}
+            </h3>
 
-          <h4 className="font-medium mb-1">
-            {review?.reviewType === "EXPERIENCE"
-              ? "Experience"
-              : "Clothing Quality"}{" "}
-            feedback
-          </h4>
-          <p className="mb-2">&quot;{review?.message}&quot;</p>
+            <h4 className="font-medium mb-1">
+              {review?.reviewType === "EXPERIENCE"
+                ? "Experience"
+                : "Clothing Quality"}{" "}
+              feedback
+            </h4>
+            <p className="mb-2">&quot;{review?.message}&quot;</p>
 
-          <p className="text-sm mb-4">
-            {formatDate(review?.createdAt, "MMM d, yyyy")} - Present
-          </p>
-          <ReviewRatingStars small rating={review?.rating} disabled/>
-          <Separator className="bg-border/50  my-2" />
-          <div className="flex  justify-between text-sm gap-8">
-            <p className="font-semibold">
-              {getCurrencySymbol(review?.project?.currency)}
-              {formatCurrencyValue(review?.project?.approvedBudget)}
+            <p className="text-sm mb-4">
+              {formatDate(review?.createdAt, "MMM d, yyyy")} - Present
             </p>
-            <button className="font-bold text-primary">View details</button>
+            <ReviewRatingStars small rating={review?.rating} disabled />
+            <Separator className="bg-border/50  my-2" />
+            <div className="flex  justify-between text-sm gap-8">
+              <p className="font-semibold">
+                {getCurrencySymbol(review?.project?.currency)}
+                {formatCurrencyValue(review?.project?.approvedBudget)}
+              </p>
+              <Link
+                href={href || "#"}
+                className={cn(
+                  "font-bold text-primary",
+                  !href && "text-muted cursor-not-allowed",
+                )}
+              >
+                View details
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
