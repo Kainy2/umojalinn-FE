e51f2e69d92360a27e734d6e2cc46ad1114c7934
droxy <chiomaotu22@gmail.com>
2025-03-13 11:30:10 +0100

    fix: project in use styling

--- src/components/custom/card/SizingTemplate.tsx
+++ src/components/custom/card/SizingTemplate.tsx
@@ -14,9 +14,9 @@ const SizingTemplateCard = (props: { template: UmojaLinnSizingTemplate }) => {
   const isBuyer = session?.user?.profileRole === "BUYER";
 
   const inUse = template?.status === "IN_USE";
-  const isDraft = template?.status === "DRAFT"
+  const isDraft = template?.status === "DRAFT";
   const projectInUse = template?.projects?.find(
-    (temp) => temp?.status !== "COMPLETED"
+    (temp) => temp?.status !== "COMPLETED",
   );
 
   return (
@@ -36,8 +36,8 @@ const SizingTemplateCard = (props: { template: UmojaLinnSizingTemplate }) => {
           className={cn(
             "absolute bottom-0 p-4 backdrop-blur-md bg-white/30 border-t-1 border-white/50 w-full",
             isBuyer &&
-            inUse &&
-            "h-full border-none flex flex-col items-center justify-center "
+              inUse &&
+              "h-full border-none flex flex-col items-center justify-center ",
           )}
         >
           {isBuyer ? (
@@ -45,14 +45,19 @@ const SizingTemplateCard = (props: { template: UmojaLinnSizingTemplate }) => {
               <h2 className="text-subtitle-2 font-bold text-center truncate w-full">
                 {template?.name}
               </h2>
-              {(inUse || isDraft) && <p className="text-primary font-semibold">{isDraft ? "Draft" : "In use"}</p>}
+              {(inUse || isDraft) && (
+                <p className="text-primary font-semibold">
+                  {isDraft ? "Draft" : "In use"}
+                </p>
+              )}
             </>
           ) : (
             <div className="flex gap-2 text-left items-center">
               <Image
                 alt=""
                 src={
-                  template?.buyer?.user?.profilePhotoUri || "/img/webp/user.webp"
+                  template?.buyer?.user?.profilePhotoUri ||
+                  "/img/webp/user.webp"
                 }
                 height={150}
                 width={150}
@@ -75,7 +80,7 @@ const SizingTemplateCard = (props: { template: UmojaLinnSizingTemplate }) => {
           )}
         </div>
         {isBuyer && inUse && !!projectInUse && (
-          <span className="absolute top-2 left-2 px-4 py-2 bg-primary text-white">
+          <span className="absolute top-2 left-2 px-2 py-1 bg-primary rounded-full text-sm max-w-[50%] truncate text-white ">
             {projectInUse?.title}
           </span>
         )}
