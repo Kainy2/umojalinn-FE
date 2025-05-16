a72ad3c4cb2901a377540910145b8c10faaa82b8
Kainy <iartkhay@gmail.com>
2025-05-16 22:33:26 +0100

    undo fund alert fix

--- src/app/(dashboard)/(corner)/(dashboard-home-welcome-name)/layout.tsx
+++ src/app/(dashboard)/(corner)/(dashboard-home-welcome-name)/layout.tsx
@@ -1,11 +1,9 @@
 import WelcomeName from "@/section/dashboard/home/WelcomeName";
-import FundProjectAlert from "@/section/dashboard/project/FundAlert";
 import React from "react";
 
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <FundProjectAlert />
       <WelcomeName />
       {children}
     </>
