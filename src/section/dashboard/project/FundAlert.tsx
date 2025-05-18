2e9cfed8d63004fb30325630df37a8f1204846b8
Kainy <iartkhay@gmail.com>
2025-05-18 05:24:22 +0100

    fix fund alert

--- src/section/dashboard/project/FundAlert.tsx
+++ src/section/dashboard/project/FundAlert.tsx
@@ -8,10 +8,11 @@ import {
   useGetProjectMilestones,
 } from "@/tanstack/hooks/useProject";
 import { AlertTriangle } from "lucide-react";
-import { useParams } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import React, { useMemo } from "react";
 
 const FundProjectAlert = () => {
+  const pathname=usePathname();
   const params = useParams<{ id: string }>();
   const { data: projectData } = useGetProjectById(params?.id);
 
@@ -25,38 +26,41 @@ const FundProjectAlert = () => {
     [projectMilestonesData?.data?.data]
   );
 
-  if (
-    projectData?.data?.data?.fundStatus === "AWAITING_FUND" &&
-    projectData?.data?.data?.amountFunded === 0
-  )
-    return (
-      <Alert
-        className="w-[90vw] lg:w-[70vw] mt-28 rounded-lg shadow-md shadow-error-700/25"
-        type="error"
-        icon={<AlertTriangle />}
-        title="Awaiting fund"
-        message="fund escrow to start project"
-        action={
-          <div className="flex flex-col md:flex-row gap-1">
-            <SelectFundingMethodDialog
-              id={uuidToBase62Safe(firstFundMilestone?.id || "")}
-              type="milestone"
+ const excludedPaths = ['ads', 'completed', 'bids', 'drafts'];
+  const isExcludedPath = excludedPaths.some(path => pathname.includes(path));
+  const isAwaitingFund = projectData?.data?.data?.fundStatus === "AWAITING_FUND";
+  const isUnfunded = projectData?.data?.data?.amountFunded === 0;
+
+  if (isExcludedPath || !isAwaitingFund || !isUnfunded) return null;
+
+  return (
+    <Alert
+      className="w-[90vw] lg:w-[70vw] mt- mb-8 rounded-lg shadow-md shadow-error-700/25"
+      type="error"
+      icon={<AlertTriangle />}
+      title="Awaiting fund"
+      message="fund escrow to start project"
+      action={
+        <div className="flex flex-col md:flex-row gap-1">
+          <SelectFundingMethodDialog
+            id={uuidToBase62Safe(firstFundMilestone?.id || "")}
+            type="milestone"
+          >
+            <Button
+              className="w-full md:w-auto"
+              variant="outline"
+              disabled={!firstFundMilestone}
             >
-              <Button
-                className="w-full md:w-auto"
-                variant="outline"
-                disabled={!firstFundMilestone}
-              >
-                Fund Milestone
-              </Button>
-            </SelectFundingMethodDialog>
-            <SelectFundingMethodDialog id={params?.id} type="project">
-              <Button className="w-full md:w-auto">Fund Project</Button>
-            </SelectFundingMethodDialog>
-          </div>
-        }
-      />
-    );
+              Fund Milestone
+            </Button>
+          </SelectFundingMethodDialog>
+          <SelectFundingMethodDialog id={params?.id} type="project">
+            <Button className="w-full md:w-auto">Fund Project</Button>
+          </SelectFundingMethodDialog>
+        </div>
+      }
+    />
+  );
 };
 
 export default FundProjectAlert;
