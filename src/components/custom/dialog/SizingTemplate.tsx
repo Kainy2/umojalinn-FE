743088adba3b1d546b5722f06ac751cff74db218
Kainy <iartkhay@gmail.com>
2025-05-27 02:38:20 +0100

    finish template things

--- src/components/custom/dialog/SizingTemplate.tsx
+++ src/components/custom/dialog/SizingTemplate.tsx
@@ -5,275 +5,70 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { DialogProps } from "@radix-ui/react-dialog";
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-  useRef,
-} from "react";
 import TextField from "../input/TextField";
 import TabButtonSelect from "../tab/ButtonSelect";
 import CustomTab from "../tab";
-import {
-  ALL_SIZING_TEMPLATES,
-  FEMALE_SIZING_TEMPLATE,
-  MALE_SIZING_TEMPLATE,
-} from "@/constant/sizingTemplate";
 import SizingTemplateInputField from "../input/SizingTemplateField";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
-  UmojaLinnFemaleSizingTemplateProps,
-  UmojaLinnMaleSizingTemplateProps,
   UmojaLinnSizingTemplate,
 } from "@/types/project";
-import { parseStringToNumber } from "@/lib/utils";
-import {
-  useCreateSizingTemplate,
-  useGetSizingTemplateById,
-  useRequestChangeSizingTemplate,
-  useUpdateSizingTemplate,
-} from "@/tanstack/hooks/useSizingTemplates";
-import { getSizingTemplateUpdateProps } from "@/lib/project";
 import { Skeleton } from "@/components/ui/skeleton";
-import { useGetMe } from "@/tanstack/hooks/useUser";
-import { useSession } from "next-auth/react";
 import VerifyDialog from "./Verify";
 import TextAreaField from "../input/TextAreaField";
 import RequestSizingTemplateViewCard from "../card/RequestSIzingTemplateView";
+import { SizingTemplateDialogProps, useSizingTemplateDialog } from "@/hooks/use-sizing-template";
+
+
 
 const SizingTemplateDialog = (
-  props: DialogProps & {
-    id?: string;
-    handleSuccess?: (template?: UmojaLinnSizingTemplate) => void;
-    type?: "CREATE" | "DRAFT-EDIT" | "DESIGNER-VIEW" | "BUYER-VIEW";
-  },
+  props: SizingTemplateDialogProps,
 ) => {
-  const [previewImage, setPreviewImage] = useState<string | null>(null);
-  const [open, setOpen] = useState(false);
-  const [highlighted, setHighlighted] = useState<
-    | keyof (UmojaLinnFemaleSizingTemplateProps &
-        UmojaLinnMaleSizingTemplateProps)
-    | null
-  >(null);
-  const [value, setValue] = useState<
-    Partial<
-      UmojaLinnFemaleSizingTemplateProps & UmojaLinnMaleSizingTemplateProps
-    >
-  >({});
-  const [gender, setGender] =
-    useState<UmojaLinnSizingTemplate["gender"]>("MALE");
-  const [unit, setUnit] = useState<UmojaLinnSizingTemplate["unit"]>("CM");
-  const [name, setName] = useState<string>("");
-  const [recommendationMode, setRecommendationMode] = useState(false);
-  const [openRequestChangesDialog, setOpenRequestChangesDialog] =
-    useState(false);
 
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
-
-  const { isPending: loadingMe } = useGetMe();
-  const { data: session } = useSession();
-  const { data: sizingTemplateData, isPending: isLoadingSizingTemplate } =
-    useGetSizingTemplateById(props?.id);
-    const {
-      mutate: requestChangeOnSizingTemplate,
-      isPending: isRequestingChangeOnSizingTemplate,
-    } = useRequestChangeSizingTemplate(props?.id, {
-      onSuccess() {
-        setReviewsEdit({});
-        setRecommendationMode(false);
-      },
-    });
-    
-  const TEMPLATE =
-    gender === "FEMALE" ? FEMALE_SIZING_TEMPLATE : MALE_SIZING_TEMPLATE;
-  const type =
-		props.type || session?.user.profileRole === "BUYER"
-			? sizingTemplateData?.data.data.status !== "IN_USE"
-				? "DRAFT-EDIT"
-				: "BUYER-VIEW"
-			: "DESIGNER-VIEW";
-  const noOfInputs = TEMPLATE?.length;
-
-
-  // Function to handle the Enter key press
-  const handleKeyPress = (
-    index: number,
-    event: React.KeyboardEvent<HTMLInputElement>,
-  ) => {
-    if (event.key === "Enter") {
-      // Move focus to the next input, if any
-      const nextIndex = index + 1;
-      if (nextIndex < noOfInputs) {
-        inputRefs.current[nextIndex]?.focus();
-      }
-    }
-  };
-
-  const [reviewsEdit, setReviewsEdit] = useState<
-    Partial<
-      Record<
-        keyof (UmojaLinnFemaleSizingTemplateProps &
-          UmojaLinnMaleSizingTemplateProps),
-        string
-      >
-    >
-  >({});
-
-  const handleChangeValuesByUnit = (
-    prevUnit: UmojaLinnSizingTemplate["unit"],
-    finalUnit: UmojaLinnSizingTemplate["unit"],
-  ) => {
-    if (prevUnit !== finalUnit) {
-      const conversionFactor = prevUnit === "CM" ? 0.393701 : 2.54;
-      setValue((prev) =>
-        Object.keys(prev).reduce((acc, key) => {
-          const typedKey = key as keyof Partial<
-            UmojaLinnFemaleSizingTemplateProps &
-              UmojaLinnMaleSizingTemplateProps
-          >;
-          const currentValue = prev[typedKey] || 0;
-          const convertedValue = currentValue * conversionFactor;
-          // Convert each value
-          acc[typedKey] = Number.isInteger(convertedValue)
-            ? convertedValue
-            : parseFloat(convertedValue.toFixed(2));
-          return acc;
-        }, {} as Partial<UmojaLinnFemaleSizingTemplateProps & UmojaLinnMaleSizingTemplateProps>),
-      );
-    }
-  };
-
-  const handleReviewsEditChange = useCallback(
-    (
-        props:
-          | keyof (UmojaLinnFemaleSizingTemplateProps &
-              UmojaLinnMaleSizingTemplateProps)
-          | null,
-      ) =>
-      (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        if (props)
-          setReviewsEdit((prev) => ({ ...prev, [props]: e?.target?.value }));
-      },
-    [],
-  );
-
-  const [hasLiveProject, isDraft] = useMemo(() => {
-    return [
-      !!sizingTemplateData?.data?.data?.projects?.some(
-        (project) => project?.status === "LIVE",
-      ),
-      sizingTemplateData?.data?.data?.status === "DRAFT",
-    ];
-  }, [sizingTemplateData?.data?.data]);
-
-  const highlightedSizingName = useMemo(
-    () =>
-      (highlighted &&
-        ALL_SIZING_TEMPLATES?.find?.((value) => highlighted === value?.prop)
-          ?.name) ||
-      "",
-    [highlighted],
-  );
-
-  useEffect(() => {
-    if (sizingTemplateData?.data?.data) {
-      const {
-        gender,
-        name,
-        unit,
-        id,
-        createdAt,
-        buyerId,
-        buyer,
-        updatedAt,
-        status,
-        projects,
-        ...templateDetails
-      } = sizingTemplateData?.data?.data;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const _ = {
-        id,
-        createdAt,
-        buyerId,
-        buyer,
-        updatedAt,
-        status,
-        projects,
-      };
-      setValue(templateDetails);
-      setName(name);
-      setGender(gender);
-      setUnit(unit);
-    }
-  }, [sizingTemplateData?.data?.data]);
-
-  const handleSuccess = (template: UmojaLinnSizingTemplate) => {
-    setOpen(false);
-    props.onOpenChange?.(false);
-    props?.handleSuccess?.(template);
-  };
-
-  const { mutate: createSizingTemplate, isPending: isCreatingSizingTemplate } =
-    useCreateSizingTemplate({
-      onSuccess(data) {
-        handleSuccess(data?.data?.data);
-      },
-    });
-
-  const { mutate: updateSizingTemplate, isPending: isUpdatingSizingTemplate } =
-    useUpdateSizingTemplate(props?.id, {
-      onSuccess(data) {
-        handleSuccess(data?.data?.data);
-      },
-    });
-
-
-
-  const handleChange =
-    (
-      prop: keyof Partial<
-        UmojaLinnFemaleSizingTemplateProps & UmojaLinnMaleSizingTemplateProps
-      >,
-    ) =>
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setValue((prev) => ({
-        ...prev,
-        [prop]: parseStringToNumber(e.target.value)?.value || 0,
-      }));
-    };
-
-  const handleSubmit = (shouldGoLive?: true) => {
-    const sizingTemplateProps: Partial<
-      UmojaLinnSizingTemplate & {
-        shouldGoLive?: true;
-      }
-    > = {
-      ...getSizingTemplateUpdateProps(gender, value),
-      gender,
-      name,
-      unit,
-      shouldGoLive,
-    };
-
-    (props?.id ? updateSizingTemplate : createSizingTemplate)(
-      sizingTemplateProps,
-    );
-  };
-
-  const loading = props?.id
-    ? isUpdatingSizingTemplate || isRequestingChangeOnSizingTemplate
-    : isCreatingSizingTemplate;
-
-  const isDesigner = session?.user?.profileRole === "DESIGNER";
-
+const {
+    loading,
+    isDesigner,
+    highlightedSizingName,
+    handleReviewsEditChange,
+    handleChange,
+    handleSubmit,
+    sizingTemplateResult,
+    reviewsEdit,
+    isDraft,
+    hasLiveProject,
+    TEMPLATE,
+    open,
+    setOpen,
+    name,
+    gender,
+    unit,
+    value,
+    highlighted,
+    setHighlighted,
+    setGender,
+    setName,
+    setUnit,
+    handleChangeValuesByUnit,
+    recommendationMode,
+    openRequestChangesDialog,
+    setOpenRequestChangesDialog,
+    setRecommendationMode,
+    loadingMe,
+    isLoadingSizingTemplate,
+    requestChangeOnSizingTemplate,
+    // type,
+    handleKeyPress,
+    previewImage,
+    setPreviewImage,
+    inputRefs,
+    modalType
+  } = useSizingTemplateDialog(props);
+ 
   // LOADING THE SIZING TEMPLATE DATA
   if (
     props?.id &&
-    (isLoadingSizingTemplate || loadingMe || !sizingTemplateData?.data?.data)
+    (isLoadingSizingTemplate || loadingMe || !sizingTemplateResult)
   ) {
     return (
       <Dialog open={open} onOpenChange={setOpen} {...props}>
@@ -297,10 +92,11 @@ const SizingTemplateDialog = (
     // !props?.id ||
     // (
     //   !hasLiveProject &&
-    //   sizingTemplateData?.data?.data?.buyerId ===
+    //   sizingTemplateResult?.buyerId ===
     //     meData?.data?.data?.buyerProfile?.id)
 
-    ["CREATE", "DRAFT-EDIT", "BUYER-VIEW"].includes(type)
+    // modalType === "EDIT"
+    ["VIEW-ONLY", "EDIT"].includes(modalType)
   ) {
     return (
       <Dialog open={open} onOpenChange={setOpen} {...props}>
@@ -365,15 +161,15 @@ const SizingTemplateDialog = (
             </div>
             <div className="max-h-[50vh] overflow-scroll">
               {TEMPLATE.map((template, index) => {
-                const isNotCreateOrDraft = !(props.type === "CREATE" || props.type === "DRAFT-EDIT");
-                const reviewValue =(recommendationMode
+                const isNotEdit = modalType !== "EDIT"
+                const reviewValue = (recommendationMode
                   ? reviewsEdit?.[template.prop]
-                  : undefined) ||
-                  sizingTemplateData?.data?.data?.metadata?.reviews?.[template.prop];
+                  : undefined) ??
+                  sizingTemplateResult?.metadata?.reviews?.[template.prop];
               
                 return (
                   <SizingTemplateInputField
-                    disabled={isNotCreateOrDraft && !reviewValue}
+                    disabled={isNotEdit && !reviewValue}
                     onValueChange={handleChange(template.prop)}
                     value={value?.[template.prop] ?? 0}
                     unit={unit}
@@ -426,13 +222,13 @@ const SizingTemplateDialog = (
                 />
               )}
               {highlighted &&
-                sizingTemplateData?.data?.data?.metadata?.reviews?.[
+                sizingTemplateResult?.metadata?.reviews?.[
                   highlighted
                 ] && (
                   <RequestSizingTemplateViewCard
                     title={highlightedSizingName}
                     review={
-                      sizingTemplateData?.data?.data?.metadata?.reviews?.[
+                      sizingTemplateResult?.metadata?.reviews?.[
                         highlighted
                       ]
                     }
@@ -532,7 +328,7 @@ const SizingTemplateDialog = (
                     (recommendationMode
                       ? reviewsEdit?.[template.prop]
                       : undefined) ||
-                    sizingTemplateData?.data?.data?.metadata?.reviews?.[
+                    sizingTemplateResult?.metadata?.reviews?.[
                       template.prop
                     ],
                   img: template?.img,
@@ -604,14 +400,14 @@ const SizingTemplateDialog = (
           {!hasLiveProject &&
             highlighted &&
             (reviewsEdit?.[highlighted] ||
-              sizingTemplateData?.data?.data?.metadata?.reviews?.[
+              sizingTemplateResult?.metadata?.reviews?.[
                 highlighted
               ]) && (
               <RequestSizingTemplateViewCard
                 title={highlightedSizingName}
                 review={
                   reviewsEdit?.[highlighted] ||
-                  sizingTemplateData?.data?.data?.metadata?.reviews?.[
+                  sizingTemplateResult?.metadata?.reviews?.[
                     highlighted
                   ] ||
                   ""
@@ -656,3 +452,5 @@ const SizingTemplateDialog = (
 };
 
 export default SizingTemplateDialog;
+
+
